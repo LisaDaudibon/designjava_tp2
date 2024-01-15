af551467c42fd98b882202af64e1f9447a0669fa
Lisa Daudibon <lisa.daudibon@outlook.fr>
2024-01-15 13:22:17 +0100

    :sparkles: LD my first attempt

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,41 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
+public static void PriceIsRight() {
+    int randomNum = (int)(Math.random() * 101);
 
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Vous devez deviner un chiffre aléatoire entre 0 et 100. " +
+            "Donnez votre première estimation : ");
+
+    int p = sc.nextInt();
+
+
+    for (int i = 0; i < 5; i++) {
+        if (p > 0 && p < 100) {
+            if (p == randomNum) {
+                System.out.print("Vous avez trouvé le juste prix");
+                break;
+            } else {
+                System.out.print("Veuillez retenter votre chance :");
+                p = sc.nextInt();
+            }
+        } else if (p < 0) {
+            System.out.print("Veuillez taper un chiffre entre 0 et 100, les nombres négatifs ne sont pas pris en compte");
+            p = sc.nextInt();
+        } else {
+            System.out.print("Veuillez taper un chiffre entre 0 et 100, les nombres supérieurs à 100 ne sont pas pris en compte");
+        }
+
+    }
+    System.out.println(randomNum);
+}
 
 public class Main {
     public static void main(String[] args) {
-
+        PriceIsRight();
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
     }
 }
